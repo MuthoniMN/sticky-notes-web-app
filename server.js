e565575c52101b72f68b057c6ab6b42c0c3967c2
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-07-26 13:28:12 +0300

    deleting notes using an id

--- server.js
+++ server.js
@@ -37,6 +37,12 @@ app.get('/api/notes/:id', (req, res) => {
     }
 })
 
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    notes = notes.filter(note => note.id != id)
+    res.status(204).end()
+})
+
 const PORT = 4444
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
