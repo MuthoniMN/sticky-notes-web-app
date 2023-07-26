a2f098911eabdbec188afd7d5f7d0b2320840b88
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-07-26 23:53:01 +0300

    slight modifications on post

--- server.js
+++ server.js
@@ -55,8 +55,16 @@ const generateId = () => {
 }
 
 app.post('/api/notes', (req, res) => {
-    const note = req.body
-    note.id = generateId()
+    if (!body.content) {
+        return response.status(400).json({ 
+          error: 'content missing' 
+        })
+    }
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
     console.log(note)
     res.json(note)
 })
