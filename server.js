9d0fe5c1681434e54501101b1f1b1fbc8417bdf3
MuthoniMN <105012834+MuthoniMN@users.noreply.github.com>
2023-07-26 12:18:07 +0300

    getting notes using an id

--- server.js
+++ server.js
@@ -3,29 +3,39 @@ const app = express()
 
 let notes = [
     {
-      id: 1,
-      content: "HTML is easy",
-      important: true
+        id: 1,
+        content: "HTML is easy",
+        important: true
     },
     {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
     },
     {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
     }
-  ]
+]
 
-  app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-  })
+})
 
-  app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.json(notes)
-  })
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    const note = notes.find(note => note.id == id)
+    if (note) {
+        res.json(note)
+    } else {
+        res.status(404).end("Not Found")
+    }
+})
 
 const PORT = 4444
 
